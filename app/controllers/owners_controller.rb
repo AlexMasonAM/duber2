b05cfaf4bba1cc60e263c2bae3555a13f5cd7694
ramiburpee <rburpee@gmail.com>
2015-01-31 21:08:36 -0800

    Add show, new and create actions to owners controller

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -4,5 +4,21 @@ class OwnersController < ApplicationController
     @owners = Owner.all
   end
 
+  def show
+    @owner = Owner.find(params[:id])
+  end
+
+  def new
+    @owner = Owner.new
+  end
+
+  def create
+    @owner = Owner.new(params.require(:owner).permit(:name, :address))
+    if @owner.save
+      redirect_to owners_path
+    else
+      render :new
+    end
+  end
 
 end
